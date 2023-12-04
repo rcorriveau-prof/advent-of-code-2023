19345cbf90c481b57c6e6466db864905574c6386
rcorriveau-prof <119197090+rcorriveau-prof@users.noreply.github.com>
2023-12-04 17:02:13 -0500

    Début du jour 2.

---
+++ project_template/part2.py
@@ -0,0 +1,17 @@
+def fn_lire_data(nom_data: str) -> list:
+    with open(f"{nom_data}_input.txt", "r") as puzzle_input:
+        return puzzle_input.read().split("\n")
+
+
+def fn_prep_data(p_puzzle_data: any) -> any:
+    return any
+
+
+def do_solution_2() -> int:
+    puzzle_data = fn_lire_data("puzzle")
+    ls_lignes = fn_prep_data(puzzle_data)
+    return int
+
+
+if __name__ == "__main__":
+    print(do_solution_2())
