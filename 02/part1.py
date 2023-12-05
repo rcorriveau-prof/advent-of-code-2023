385b23fab7decf16f08299ea4d76c24663d80854
rcorriveau-prof <119197090+rcorriveau-prof@users.noreply.github.com>
2023-12-05 09:53:32 -0500

    extraction de données réuss

--- 02/part1.py
+++ 02/part1.py
@@ -1,15 +1,50 @@
+# only 12 red cubes, 13 green cubes, and 14 blue cubes
+# What is the sum of the IDs of those games ?
+# Réponse 8 pour le test input
+
+
 def fn_lire_data(nom_data: str) -> list:
     with open(f"{nom_data}_input.txt", "r") as puzzle_input:
         return puzzle_input.read().split("\n")
 
 
-def fn_prep_data(p_puzzle_data: any) -> any:
-    return any
+def fn_prep_data(p_puzzle_data: list) -> dict:
+    dt_data = {}
+    for ligne in p_puzzle_data:
+        dt_game = {}  # Une game
+        game, cubes = ligne.split(":")
+        # Extraire le game ID, en str
+        game_id = ""
+        for num in game:
+            if num.isdigit():
+                game_id += num
+        # Extraire les cubes, par tour
+        for num_tour, tour in enumerate(cubes.split(";")):
+            dt_tour = {}
+            for cube in tour.split(","):
+                nb_cubes = ""
+                couleur_cubes = ""
+                for car in cube:
+                    if car.isdigit():
+                        nb_cubes += car
+                    else:
+                        couleur_cubes += car
+                dt_tour.update({couleur_cubes.strip(): int(nb_cubes.strip())})
+            dt_game.update({f"{num_tour + 1}": dt_tour})
+        dt_data.update({game_id: dt_game})
+    return dt_data
 
 
 def do_solution_1() -> int:
-    puzzle_data = fn_lire_data("puzzle")
+    puzzle_data = fn_lire_data("test")
     ls_lignes = fn_prep_data(puzzle_data)
+    dt_games = {"1":
+                {
+                "tour1": {"red": 0, "green": 0, "blue": 0},
+                "tour2": {"red": 0, "green": 0, "blue": 0},
+                "tour3": {"red": 0, "green": 0, "blue": 0}
+                }
+    }
     return int
 
 
